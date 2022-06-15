fcec9982e03255e6f2e356b21e0e98752c078cd6
Bilaal Ahmad <bilaal-ahmad@hotmail.co.uk>
2022-06-15 13:23:48 +0100

    Prepare for deployment�

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+# Blog - Learning NextJS
+
+A simple blog created by following [Next.js interactive tutorial](https://nextjs.org/learn/basics/create-nextjs-app).
+
+It covers the basics of Next including:
+
+- Static site generation
+- Data fetching
+- Basic routing
+- Dynamic routing
+- CSS modules
+- Deployment
+- TypeScript types
+
+Touches upon topics like:
+
+- Server-side rendering
+- SEO
+- API routes
---
+++ pages/posts/[id].js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+// Components
+import Head from 'next/head';
+
+import Layout from '../../components/layout';
+import Date from '../../components/date';
+
+// Utils
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+// Styles
+import utilStyles from '../../styles/utils.module.css';
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
+      <article>
+        <header className="post-head">
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+        </header>
+
+        <section
+          className="post-body"
+          dangerouslySetInnerHTML={{ __html: postData.content }}
+        />
+      </article>
+    </Layout>
+  );
+}
